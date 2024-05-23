26e529086489aa09c93036e4596f6ab45dc9d531
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-05-23 18:26:01 +0200

    Add select type in  edit page

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -77,6 +77,18 @@
 
             </div>
 
+            <div class="mb-3">
+                <label for="type_id" class="form-label">Type</label>
+                <select class="form-select" name="type_id" id="type_id">
+                    <option selected disabled>Select a type</option>
+                    @foreach ($types as $type)
+                        <option value="{{ $type->id }}" {{ $type->id == old('type_id') ? 'selected' : '' }}>
+                            {{ $type->name }} </option>
+                    @endforeach
+
+                </select>
+            </div>
+
 
             <div class="mb-3">
                 <label for="description" class="form-label">Description of project</label>
