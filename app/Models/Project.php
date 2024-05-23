3a84e1d9ce771b107236c29d46e5b17bc5895bc9
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-05-23 17:53:21 +0200

    Add table and model relationship

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,9 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 
 class Project extends Model
 {
     use HasFactory;
-    protected $fillable=['title', 'slug', 'subtitle', 'description', 'image','tecnology' ,'url', 'image'];
+    protected $fillable=['title', 'slug', 'subtitle', 'description', 'image','tecnology' ,'url', 'image', 'type_id'];
+
+  /**
+   * Get the type that owns the Project
+   *
+   * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+   */
+  public function type(): BelongsTo
+  {
+      return $this->belongsTo(Type::class, 'foreign_key', 'other_key');
+  }
 }
